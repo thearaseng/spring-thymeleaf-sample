b4297aac1eb3847af230c6c194809dae1e2995fd
Theara Seng <seng.theara.kh@gmail.com>
2018-02-09 16:29:21 +0700

    add itextkhmer library

---
+++ src/main/java/com/jsaccounting/library/KhmerLigature.java
@@ -0,0 +1,18 @@
+package com.jsaccounting.library;
+
+import com.itextpdf.text.pdf.BidiLine;
+import com.itextpdf.text.pdf.languages.LanguageProcessor;
+import com.jsaccounting.library.itextkhmer.render.UnicodeRender;
+
+public class KhmerLigature implements LanguageProcessor {
+
+    public String process(String s) {
+        UnicodeRender khmerRender = new UnicodeRender();
+        return BidiLine.processLTR(khmerRender.render(s), 2, 0);
+    }
+
+    @Override
+    public boolean isRTL() {
+        return false;
+    }
+}
